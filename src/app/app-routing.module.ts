ce9972e3e05129ed4acb2b99e641a796ec697abe
Kgothatso <kay121407@gmail.com>
2020-12-02 12:35:14 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './page/menu/tabs/tabs.page';
+import { SidemenuPage } from './page/menu/sidemenu/sidemenu.page';
 
 const routes: Routes = [
   {
@@ -11,6 +13,49 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+
+  {
+    path: 'detail',
+    loadChildren: () => import('./page/detail/detail.module').then( m => m.DetailPageModule)
+  },
+  {
+    path: 'sidemenu', component: SidemenuPage, children: [
+
+     
+
+      {
+        path: 'tabs',
+        component: TabsPage,
+        children: [
+
+          {
+            path: 'home',
+            loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+          },
+          {
+            path: '',
+            redirectTo: 'home',
+            pathMatch: 'full'
+          },
+    
+          {
+            path: 'profile',
+            loadChildren: () => import('./page/profile/profile.module').then( m => m.ProfilePageModule)
+          },
+          {
+            path: 'contact',
+            loadChildren: () => import('./page/contact/contact.module').then( m => m.ContactPageModule)
+          },
+    
+
+        ]
+      },
+
+    ]
+   
+  },
+ 
+ 
 ];
 
 @NgModule({
